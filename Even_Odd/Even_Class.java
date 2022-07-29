a7d1c70bb41534422ebe58f714bb2a07e4532e1e
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-29 19:24:38 +0530

    Update Even_Class.java

--- Even_Odd/Even_Class.java
+++ Even_Odd/Even_Class.java
@@ -1,24 +1,23 @@
 package Threads;
-
 import java.util.Scanner;
 
 public class Even_Class  extends Thread {
-
+	// global variable
 	Scanner sc = new Scanner(System.in);
 	int s,l,i;
-	public void accept()
+	public void accept() // accept method calling by the another class 
 	{
-		System.out.println("Enter the Start Value : ");
+            System.out.println("Enter the Start Value : "); // taking a first value from the user
 	    s = sc.nextInt();
-	    System.out.println("Enter the Last Value : ");
+	    System.out.println("Enter the Last Value : ");  // taking a last value from the user
 	    l = sc.nextInt();
 	}
-	public void run()
+	public void run() // run method calling by the another class
 	{
-		accept();
+	    accept();
 	    for(i=s; i<=l; i++)
 	    {
-	    	if(i%2 == 0)
+	    	if(i%2 == 0) // checking a even condition
 	    	{
 	    		System.out.println(i +" Even");
 	    	}

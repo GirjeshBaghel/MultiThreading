50428a1b4364f8c21e60076efa0451a59441d4b0
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-29 19:30:57 +0530

    Update Main_Class.java

--- Even_Odd/Main_Class.java
+++ Even_Odd/Main_Class.java
@@ -1,12 +1,12 @@
 package Threads;
-
+// main class that calls the another classes
 public class Main_Class  {
-	public static void main(String[] args)
+	public static void main(String[] args) // main method
 	{
-		Odd_Class ob = new Odd_Class();
-		Even_Class ob1 = new Even_Class();		
-		ob.start();	
-		ob1.start();
+		Odd_Class ob = new Odd_Class(); // odd class object creating
+		Even_Class ob1 = new Even_Class(); // even class object creating	
+		ob.start(); // run method is calling
+		ob1.start(); // run method is calling
 		
 	}
 

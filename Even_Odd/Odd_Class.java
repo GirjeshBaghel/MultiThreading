032b5c554a858d70e0b7b8438378b4b87318ebff
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-29 19:35:37 +0530

    Update Odd_Class.java

--- Even_Odd/Odd_Class.java
+++ Even_Odd/Odd_Class.java
@@ -1,18 +1,16 @@
 package Threads;
-
 public class Odd_Class extends Even_Class {
 	
-	public void run()
+	public void run() // run method is calling by the another
 	{
-		accept();
+		accept(); // accept  method calls  to the another class 
 		for(i=s; i<=l; i++)
-	    {			
-	    	if(i%2 != 0)
+	    	{			
+	    	if(i%2 != 0) // logic of odd no 
 	    	{
-	    		System.out.println(i+" Odd");
+	    		System.out.println(i+" Odd"); // printing a odd no
 	    	}
 	    	
 	    }
 	}
-
 }

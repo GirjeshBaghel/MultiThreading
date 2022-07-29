f3855fb020dfbed9a0c351b1dd4bb93de638eb3e
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-29 19:15:13 +0530

    Update Even_Odd.java

--- Even_Odd.java
+++ Even_Odd.java
@@ -1,49 +1,47 @@
 package Threads;
-
 import java.util.Scanner;
 
 public class Even_Odd  extends Thread{
+	// global variable
 	Scanner sc = new Scanner(System.in);
 	int s,l,i;
-		public static void main(String[] args)
-		{
-			Even_Odd ob = new Even_Odd();
-			ob.start();
-		}
-		
-		public void run()
-		{
-			
-			System.out.print("Enter the Start Value : ");
+				
+		public void run() // run method calling by the main method
+		{		
+		    System.out.print("Enter the Start Value : "); // taking a start value from user
 		    s = sc.nextInt();
-		    System.out.print("Enter the Last Value : ");
+		    System.out.print("Enter the Last Value : "); // taking a last value from user
 		    l = sc.nextInt();
 		    
-		    System.out.println("Even no are : ");
+		    System.out.println("Even no are : "); //printing a even no
 		    for(i=s; i<=l; i++)
 		    {
 		    	try {
-					Thread.sleep(2000);
+					Thread.sleep(2000); // sleeping a thread
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-		    	if(i%2 == 0)
+		    	if(i%2 == 0) // even no printing
 		    	{
-		    		System.out.println(i);
+		    		System.out.print(i);
 		    	}
 		    	
 		    }
-		    System.out.println("Odd no are : ");
+		    System.out.println("\nOdd no are : "); // odd no printing
 		    for(i=s; i<=l; i++)
 		    {
-		    	if(i%2 != 0)
+		    	if( %2 != 0) // checking a odd condition
 		    	{
-		    		System.out.println(i);
+		    		System.out.print(i);
 		    	}
 		    	
 		    }
-		    
 			
+		}
+		public static void main(String[] args) // main method
+		{
+			Even_Odd ob = new Even_Odd();
+			ob.start(); // run method calling
 		}
 }

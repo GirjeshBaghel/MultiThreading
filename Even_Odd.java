cef86d1e99c2260fe682a2ae77feb6354707cc83
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-08-03 18:27:15 +0530

    Update Even_Odd.java

--- Even_Odd.java
+++ Even_Odd.java
@@ -42,6 +42,8 @@ public class Even_Odd  extends Thread{
 		public static void main(String[] args) // main method
 		{
 			Even_Odd ob = new Even_Odd();
+			Even_Odd ob1 = new Even_Odd();
 			ob.start(); // run method calling
+			ob1.start(); // run method calling
 		}
 }

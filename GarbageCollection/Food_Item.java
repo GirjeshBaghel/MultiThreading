822905e01cc5b02a8fa820791197608cdd962ee5
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-08-09 22:16:46 +0530

    Update Food_Item.java

--- GarbageCollection/Food_Item.java
+++ GarbageCollection/Food_Item.java
@@ -3,42 +3,42 @@ import java.util.Scanner;
 
 public class Food_Item { 
 
-	enum food 
+	enum food  // enum creation
 	{
-		Dhosa(100),Manchurian(150),Noodles(80),SpringRoll(70),GarlicBread(90);
+		Dhosa(100),Manchurian(150),Noodles(80),SpringRoll(70),GarlicBread(90); // enum data store
 		int item;
-		food(int item)
+		food(int item) // enum constructor
 		{
-			this.item = item;
+			this.item = item;  // store a item from this keyword
 		}
 		
-		public int food_accept()
+		public int food_accept()  // food method inside a enum 
 		{
-			return item;
+			return item;  // return a item
 		}
 	}
-	public static void main(String[] args)
+	public static void main(String[] args) // main method
 	{
-		calling();
+		calling(); // calling method calling
 	}
-	public static void calling()
+	public static void calling() // calling method
 	{
-		food obj[] =food.values();
+		food obj[] =food.values();  // store a enum in a array variable
 		System.out.println("All the Items are here ");
-		for(food f:obj)
+		for(food f:obj)   // for loop is to store array in a f variable
 		{
-			System.out.println(f+" : "+f.item);
+			System.out.println(f+" : "+f.item); // printing the enum data with its price
 		}
-		System.out.print("\nEnter the item name here : ");
-		Scanner sc = new Scanner(System.in);
+		System.out.print("\nEnter the item name here : "); // taking the user choice here
+		Scanner sc = new Scanner(System.in); 
 		String choice;
 		choice = sc.next();
-		try {
-			System.out.print("The price of "+choice+" : "+food.valueOf(choice).food_accept());
+		try {  			// try block to checking the exception
+			System.out.print("The price of "+choice+" : "+food.valueOf(choice).food_accept());  // printing the user choice and price of the food
 		}
-		catch(Exception e)
+		catch(Exception e)   
 		{
-			System.out.println("Please Choose correct Item here ");
+			System.out.println("Please Choose correct Item here "); // catching the exception
 		}
 	}
 }
